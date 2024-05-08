99e4ace8373c80f684f8ff4581a36522c4cb6a76
Mark <markhjort99@gmail.com>
2024-05-08 10:08:13 +0200

    Added Tailwind to Project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import defaultConfig from "tailwindcss/defaultConfig";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "dark-blue": "var(--dark-blue)",
+      "cph-blue": "var(--cph-blue)",
+      "light-blue": "var(--light-blue)",
+      "dark-yellow": "var(--dark-yellow)",
+      "cph-yellow": "var(--cph-yellow)",
+      "light-yellow": "var(--light-yellow)",
+      "cph-white": "var(--cph-white)",
+      "grey-lighter": "var(--grey-lighter)",
+      "grey-light": "var(--grey-light)",
+      "grey-mid": "var(--grey-mid)",
+      "grey-dark": "var(--grey-dark)",
+      "grey-text": "var(--grey-text)",
+    },
+    fontFamily: {
+      sans: ['"Open Sans"', ...defaultConfig.theme.fontFamily.sans],
+    },
+  },
+  plugins: [],
+};
