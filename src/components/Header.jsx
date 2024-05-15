87aaad47c301c916fedba9b1b56abfebc934634b
Hannibal Grønne <hannibalgronne@gmail.com>
2024-05-15 10:51:35 +0200

    Reduce delay for dropdown closing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
   const mouseLeave = () => {
     timeoutRef.current = setTimeout(() => {
       setShowDropDown(null);
-    }, 600);
+    }, 300);
   };
 
   const dropDownEnter = () => {
@@ -31,61 +31,21 @@ export default function Header() {
     <header>
       <img className="logo" src="images/logo.webp" alt="" />
       <ul className="upper-nav">
-        <li
-          className="upper-li"
-          onMouseEnter={() => mouseEnter("Flyinformation")}
-          onMouseLeave={mouseLeave}
-        >
+        <li className="upper-li" onMouseEnter={() => mouseEnter("Flyinformation")} onMouseLeave={mouseLeave}>
           <a href="">Flyinformation</a>
-          {showDropDown === "Flyinformation" && (
-            <DropDown
-              item="Flyinformation"
-              onMouseEnter={dropDownEnter}
-              onMouseLeave={dropDownLeave}
-            />
-          )}
+          {showDropDown === "Flyinformation" && <DropDown item="Flyinformation" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
         </li>
-        <li
-          className="upper-li"
-          onMouseEnter={() => mouseEnter("Praktisk")}
-          onMouseLeave={mouseLeave}
-        >
+        <li className="upper-li" onMouseEnter={() => mouseEnter("Praktisk")} onMouseLeave={mouseLeave}>
           <a href="">Praktisk</a>
-          {showDropDown === "Praktisk" && (
-            <DropDown
-              item="Praktisk"
-              onMouseEnter={dropDownEnter}
-              onMouseLeave={dropDownLeave}
-            />
-          )}
+          {showDropDown === "Praktisk" && <DropDown item="Praktisk" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
         </li>
-        <li
-          className="upper-li"
-          onMouseEnter={() => mouseEnter("Parkering")}
-          onMouseLeave={mouseLeave}
-        >
+        <li className="upper-li" onMouseEnter={() => mouseEnter("Parkering")} onMouseLeave={mouseLeave}>
           <a href="">Parkering</a>
-          {showDropDown === "Parkering" && (
-            <DropDown
-              item="Parkering"
-              onMouseEnter={dropDownEnter}
-              onMouseLeave={dropDownLeave}
-            />
-          )}
+          {showDropDown === "Parkering" && <DropDown item="Parkering" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
         </li>
-        <li
-          className="upper-li"
-          onMouseEnter={() => mouseEnter("Butikker & Spisesteder")}
-          onMouseLeave={mouseLeave}
-        >
+        <li className="upper-li" onMouseEnter={() => mouseEnter("Butikker & Spisesteder")} onMouseLeave={mouseLeave}>
           <a href="">Butikker & Spisesteder</a>
-          {showDropDown === "Butikker & Spisesteder" && (
-            <DropDown
-              item="Butikker & Spisesteder"
-              onMouseEnter={dropDownEnter}
-              onMouseLeave={dropDownLeave}
-            />
-          )}
+          {showDropDown === "Butikker & Spisesteder" && <DropDown item="Butikker & Spisesteder" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
         </li>
       </ul>
       <div className="flex-right">
