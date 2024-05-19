c0263ebd984bb1b10240b5a4b4f99175c5049980
Mark <markhjort99@gmail.com>
2024-05-19 19:48:21 +0200

    Pagination og Sorting

--- src/supabase/getItems.js
+++ src/supabase/getItems.js
@@ -5,12 +5,22 @@ export const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bWRvb2dlb3RlenBmd29wdHpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3NjUyOTgsImV4cCI6MjAzMTM0MTI5OH0.1T1vtWtUFsUbnKGASiNRHdHbim6Yv8oQ7Klr1SOAzqc"
 );
 
-export async function getItems(page = 1, pageSize = 6) {
+export async function getItems(sortBy, filterBy, page = 1, pageSize = 6) {
   const from = (page - 1) * pageSize;
   const to = page * pageSize - 1;
 
-  return await supabase
+  const call = supabase
     .from("hittegodslist")
     .select("*", { count: "exact" })
     .range(from, to);
+
+  if (sortBy) {
+    call.order(sortBy, { ascending: true });
+  }
+
+  if (filterBy) {
+    call.filter("item_type", "eq", filterBy);
+  }
+
+  return await call;
 }
