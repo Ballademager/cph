326ca59545da8da335ead613de8f194471c2b24c
Hannibal Grønne <hannibalgronne@gmail.com>
2024-05-20 15:09:07 +0200

    Adjusted styles to fit prototype and added a bit mobile-styles

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -8,21 +8,20 @@ const FAQQuestion = ({ open: { open, setOpen }, question: { question, answer, te
 
   return (
     <li
-      className="shadow-xl px-10 py-8 rounded-xl cursor-pointer"
+      className="flex flex-col shadow-boxbox px-6 md:px-10 py-4 md:py-6 lg:py-8 rounded-xl cursor-pointer"
       onClick={() => {
         setOpen(isOpen ? -1 : i);
         setHeight(contentRef.current.clientHeight);
-      }}
-    >
-      <div className="flex justify-between items-center">
-        <p className="text-large font-bold">{question}</p>
+      }}>
+      <div className="flex justify-between items-center gap-2">
+        <p className="font-bold text-large sm:text-6 lg:text-5">{question}</p>
 
         <svg
-          width={50}
-          height={50}
+          width={40}
+          height={40}
           data-slot="icon"
           fill="none"
-          stroke-width="1.5"
+          stroke-width="2.5"
           stroke="currentColor"
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
@@ -30,13 +29,8 @@ const FAQQuestion = ({ open: { open, setOpen }, question: { question, answer, te
           style={{
             transform: isOpen ? `rotate(180deg)` : "rotate(0deg)",
           }}
-          className="transition-transform duration-300 ease-in-out"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m19.5 8.25-7.5 7.5-7.5-7.5"
-          ></path>
+          className="transition-transform duration-300 ease-in-out min-w-10">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"></path>
         </svg>
       </div>
 
@@ -44,10 +38,12 @@ const FAQQuestion = ({ open: { open, setOpen }, question: { question, answer, te
         className="relative overflow-hidden transition-height duration-300 ease-in-out"
         style={{
           height: isOpen ? height : 0,
-        }}
-      >
-        <div className="relative h-fit" ref={contentRef}>
-          {answer}
+        }}>
+        <div className="h-fit flex flex-col gap-6" ref={contentRef}>
+          <p className="relative text-large mt-4">{answer}</p>
+          <a className="bg-cph-blue text-cph-white font-large font-semibold px-11 py-3 rounded-lg self-start" href={hyperRef}>
+            {text}
+          </a>
         </div>
       </div>
     </li>
@@ -58,13 +54,13 @@ export function FAQ({ faqQuestions, headlineText, paragraphText }) {
   const [open, setOpen] = useState(-1);
 
   return (
-    <div className="grid py-10 w-full">
+    <div className="grid py-10 w-full gap-4">
       <div>
         <h2 className="text-5 sm:text-4 font-bold text-cph-blue py-3">{headlineText}</h2>
         <p className="text-p md:text-large">{paragraphText}</p>
       </div>
       <div>
-        <ul>
+        <ul className="grid gap-4">
           {faqQuestions.map((question, i) => (
             <FAQQuestion
               key={i}
