3d2836fc389e628effc4e4ce6fd14bae310e1187
Hannibal Grønne <hannibalgronne@gmail.com>
2024-05-20 11:20:40 +0200

    Add headercontent for screen-width< 1024px
    Headerens content er conditionally rendered baseret på skærmens bredde. Hvis skærmen er mindre end 1024px vises mobilversionen.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import DropDown from "./DropDown";
+import InnerList from "./InnerList";
 import "./Header.css";
 
 export default function Header() {
   const [showDropDown, setShowDropDown] = useState(null);
   const timeoutRef = useRef(null);
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [active, setActive] = useState(false);
 
   const mouseEnter = (item) => {
     setShowDropDown(item);
@@ -27,9 +30,25 @@ export default function Header() {
     }, 300);
   };
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenWidth(window.innerWidth);
+
+      const handleResize = () => {
+        setScreenWidth(window.innerWidth);
+      };
+
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
   return (
     <header>
-      <img className="logo" src="images/logo.webp" alt="" />
+      {screenWidth > 1024 ? (
+        <>
       <ul className="upper-nav">
         <li className="upper-li" onMouseEnter={() => mouseEnter("Flyinformation")} onMouseLeave={mouseLeave}>
           <a href="">Flyinformation</a>
@@ -55,8 +74,57 @@ export default function Header() {
           <div></div>
           <div>&#x25BC;</div>
         </button>
-        <div className="search"></div>
+            <div className="search">
+              <img src="src/images/search.svg" alt="search on site" />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="flex-right">
+          <div className="search">
+            <img src="src/images/search.svg" alt="search on site" />
+          </div>
+          <div className={"burger-menu " + (active ? "active" : "")} onClick={handleBurgerClick}>
+            <span className="bar1"></span>
+            <span className="bar2"></span>
+            <span className="bar3"></span>
+          </div>
+          <nav className={"mobile-menu " + (active ? "active" : "")}>
+            <div className="top-row">
+              <button className="language">
+                <div></div>
+                <div>&#x25BC;</div>
+              </button>
+            </div>
+            <ul className="outer-list">
+              <li className={"menu-item " + (liOneActive ? "active" : "")} onClick={handleLiOneClick}>
+                <p>Flyinformation</p>
+                <CSSTransition in={liOneActive} timeout={300} classNames="inner-list" unmountOnExit>
+                  <InnerList itemKey="flyinformationContent" />
+                </CSSTransition>
+              </li>
+              <li className={"menu-item " + (liTwoActive ? "active" : "")} onClick={handleLiTwoClick}>
+                <p>Praktisk</p>
+                <CSSTransition in={liTwoActive} timeout={300} classNames="inner-list" unmountOnExit>
+                  <InnerList itemKey="praktiskContent" />
+                </CSSTransition>
+              </li>
+              <li className={"menu-item " + (liThreeActive ? "active" : "")} onClick={handleLiThreeClick}>
+                <p>Parkering</p>
+                <CSSTransition in={liThreeActive} timeout={300} classNames="inner-list" unmountOnExit>
+                  <InnerList itemKey="parkeringContent" />
+                </CSSTransition>
+              </li>
+              <li className={"menu-item " + (liFourActive ? "active" : "")} onClick={handleLiFourClick}>
+                <p>Butikker & Spisesteder</p>
+                <CSSTransition in={liFourActive} timeout={300} classNames="inner-list" unmountOnExit>
+                  <InnerList itemKey="butikkerSpisestederContent" />
+                </CSSTransition>
+              </li>
+            </ul>
+          </nav>
       </div>
+      )}
     </header>
   );
 }
