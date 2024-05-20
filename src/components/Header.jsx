3012cfbdc1f0aa7f8708e11498835703f9a0244b
Hannibal Grønne <hannibalgronne@gmail.com>
2024-05-20 11:30:34 +0200

    Styling for mobile-header and burgermenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -76,33 +76,34 @@ export default function Header() {
 
   return (
     <header>
+      <img className="logo" src="src/images/logo.webp" alt="" />
       {screenWidth > 1024 ? (
         <>
-      <ul className="upper-nav">
-        <li className="upper-li" onMouseEnter={() => mouseEnter("Flyinformation")} onMouseLeave={mouseLeave}>
-          <a href="">Flyinformation</a>
-          {showDropDown === "Flyinformation" && <DropDown item="Flyinformation" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
-        </li>
-        <li className="upper-li" onMouseEnter={() => mouseEnter("Praktisk")} onMouseLeave={mouseLeave}>
-          <a href="">Praktisk</a>
-          {showDropDown === "Praktisk" && <DropDown item="Praktisk" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
-        </li>
-        <li className="upper-li" onMouseEnter={() => mouseEnter("Parkering")} onMouseLeave={mouseLeave}>
-          <a href="">Parkering</a>
-          {showDropDown === "Parkering" && <DropDown item="Parkering" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
-        </li>
-        <li className="upper-li" onMouseEnter={() => mouseEnter("Butikker & Spisesteder")} onMouseLeave={mouseLeave}>
-          <a href="">Butikker & Spisesteder</a>
-          {showDropDown === "Butikker & Spisesteder" && <DropDown item="Butikker & Spisesteder" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
-        </li>
-      </ul>
-      <div className="flex-right">
-        <img className="taxfree" src="images/taxfree.webp" alt="" />
-        <a href="">Log ind</a>
-        <button className="language">
-          <div></div>
-          <div>&#x25BC;</div>
-        </button>
+          <ul className="upper-nav">
+            <li className="upper-li" onMouseEnter={() => mouseEnter("Flyinformation")} onMouseLeave={mouseLeave}>
+              <a href="">Flyinformation</a>
+              {showDropDown === "Flyinformation" && <DropDown item="Flyinformation" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
+            </li>
+            <li className="upper-li" onMouseEnter={() => mouseEnter("Praktisk")} onMouseLeave={mouseLeave}>
+              <a href="">Praktisk</a>
+              {showDropDown === "Praktisk" && <DropDown item="Praktisk" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
+            </li>
+            <li className="upper-li" onMouseEnter={() => mouseEnter("Parkering")} onMouseLeave={mouseLeave}>
+              <a href="">Parkering</a>
+              {showDropDown === "Parkering" && <DropDown item="Parkering" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
+            </li>
+            <li className="upper-li" onMouseEnter={() => mouseEnter("Butikker & Spisesteder")} onMouseLeave={mouseLeave}>
+              <a href="">Butikker & Spisesteder</a>
+              {showDropDown === "Butikker & Spisesteder" && <DropDown item="Butikker & Spisesteder" onMouseEnter={dropDownEnter} onMouseLeave={dropDownLeave} />}
+            </li>
+          </ul>
+          <div className="flex-right">
+            <img className="taxfree" src="images/taxfree.webp" alt="" />
+            <a href="">Log ind</a>
+            <button className="language">
+              <div></div>
+              <div>&#x25BC;</div>
+            </button>
             <div className="search">
               <img src="src/images/search.svg" alt="search on site" />
             </div>
@@ -152,7 +153,7 @@ export default function Header() {
               </li>
             </ul>
           </nav>
-      </div>
+        </div>
       )}
     </header>
   );
